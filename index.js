2d3137741adf254988c1b8c881873547b6bb87fa
Lola <lolaabitova90@gmail.com>
2023-02-21 00:48:26 +0600

    Created an array of strings and concatenate them into one string

--- index.js
+++ index.js
@@ -97,3 +97,15 @@ console.log(sum);
 arraySum(arr);
 
 // zadacha 16
+let arrChet = [2,3,4,8,10,9,7]
+function arraySumchet(array){
+let sum = 0;
+for(let i = 0; i < array.length; i++){
+  if (array[i] % 2 === 0){
+    sum += array[i];
+  }
+    
+    }
+console.log(sum);
+}
+arraySumchet(arrChet);
